3522c5fe45efaef46c8c5b652b1e81771b4471a7
Axelle Adamkiewicz <axelle.adamkiewicz@gmail.com>
2019-04-15 11:43:38 +0200

    Adding my final work.

---
+++ src/components/History.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class History extends Component {
+    render() {
+        return (
+            <div>
+              <p className="navigation">Notre Histoire</p>
+            </div>
+        );
+    }
+}
+
+export default History;
